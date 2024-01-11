e0737bfc055c42074551953755a912753a1ac168
audi0010 <tristan.audinot@etudiant.univ-reims.fr>
2024-01-11 15:48:45 +0100

    Implementation d'une IA stupide

--- Model/Joueur.py
+++ Model/Joueur.py
@@ -1,7 +1,7 @@
 from Model.Constantes import *
 from Model.Pion import *
 from Model.Plateau import *
-
+from random import randint
 
 #
 # Ce fichier contient les fonctions gérant le joueur
@@ -124,7 +124,7 @@ def setPlacerPionJoueur(joueur: dict, fn) -> None:
     """
     Cette fonction « affecte » une fonction au joueur
 
-    :param joueur: Paramètre représenter par un dictionnaire composé d'une couleur, d'un plateau et de placer pion.
+    :param joueur: Paramètre représenté par un dictionnaire composé d'une couleur, d'un plateau et de placer pion.
     :param fn: Ce paramètre est une fonction que l'on veut affecter au joueur
     :return: Ne retourne rien
     """
@@ -134,4 +134,41 @@ def setPlacerPionJoueur(joueur: dict, fn) -> None:
         raise TypeError("setPlacerPionJoueur : le second paramètre n’est pas une fonction")
 
     joueur[const.PLACER_PION] = fn
+    return None
+
+
+def _placerPionJoueur(joueur: dict) -> int:
+    """
+    Cette fonction choisit un nombre aléatoire entre 0 et const.NB_COLUMNS – 1 tout en vérifiant que la colonne
+    correspondante peut être jouée, qu’elle n’est pas remplie et le retourne.
+
+    :param joueur: Paramètre représenté par un dictionnaire composé d'une couleur, d'un plateau et de placer pion.
+    :return: Retourne un index de colonne aléatoire ou il possible de mettre un pion.
+    """
+    if not type_joueur(joueur):
+        raise TypeError("_placerPionJoueur : Le paramètre n'est pas un joueur")
+
+    if not isRempliPlateau(joueur[const.PLATEAU]):
+        etat = True
+        while etat:
+            colonne = randint(0, const.NB_COLUMNS - 1)
+            if joueur[const.PLATEAU][0][colonne] is None:
+                return colonne
+
+
+def initialiserIAJoueur(joueur: dict, booleen: bool) -> None:
+    """
+    Cette fonction affecte la fonction _placerPionJoueur à un joueur
+
+    :param joueur: Paramètre représenté par un dictionnaire composé d'une couleur, d'un plateau et de placer pion.
+    :param booleen: Booléen indiquant si le joueur joue en premier (True) ou en second (False).
+    (sert dans une autre fonction)
+    :return: Ne retourne rien
+    """
+    if not type_joueur(joueur):
+        raise TypeError("initialiserIAJoueur : Le premier paramètre n'est pas un joueur")
+    if type(booleen) != bool:
+        raise TypeError("initialiserIAJoueur : Le second paramètre n'est pas un booléen")
+
+    setPlacerPionJoueur(joueur, _placerPionJoueur)
     return None
